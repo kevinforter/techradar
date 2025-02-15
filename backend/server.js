36da633f560d779448daa011ee0b568cdc19996e
kevinforter <49223606+kevinforter@users.noreply.github.com>
2025-02-15 18:56:42 +0100

    calling server status via route

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ const app = express();
 require('dotenv').config();
 
 const db = require('./config/database');
+const statusRoute = require('./routes/status.route');
 
 // Connect to MongoDB
 db.connect(app, `${process.env.PORT}`);
+
+// Status route
+app.use('/api/v1/status', statusRoute);
