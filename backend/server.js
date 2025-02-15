02876512fa8662d26b35b6fbff24104d8525cce7
kevinforter <49223606+kevinforter@users.noreply.github.com>
2025-02-15 21:31:39 +0100

    accept json and form input

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ require('dotenv').config();
 const db = require('./config/database');
 const statusRoute = require('./routes/status.route');
 
+// Middleware to parse JSON bodies from incoming requests
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Connect to MongoDB
 db.connect(app, `${process.env.PORT}`);
 
