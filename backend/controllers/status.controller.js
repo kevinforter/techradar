3835ce59532245162cfa91881790058090dd7a0e
kevinforter <49223606+kevinforter@users.noreply.github.com>
2025-02-15 19:20:35 +0100

    added mongoDB Status to get req

--- backend/controllers/status.controller.js
+++ backend/controllers/status.controller.js
@@ -1,6 +1,18 @@
+const mongoose = require('mongoose');
+
 const getStatus = (req, res) => {
+  const state = mongoose.connection.readyState;
+  let mongoState;
+
+  if (state === 1) {
+    mongoState = 'Connected to MongoDB';
+  } else {
+    mongoState = 'Not connected to MongoDB';
+  }
+
   const status = {
-    Status: 'Running',
+    Server: 'Running',
+    MongoDB: mongoState,
   };
   res.send(status);
 };
