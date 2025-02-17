058e640fa96a257baa6529eba493b85d60f4e228
kevinforter <49223606+kevinforter@users.noreply.github.com>
2025-02-17 17:42:01 +0100

    changed let to const

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-let authToken = (req, res, next) => {
+const authToken = (req, res, next) => {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
   if (token === null) return res.sendStatus(401);
@@ -13,7 +13,7 @@ let authToken = (req, res, next) => {
   });
 };
 
-let authRole = (roles) => {
+const authRole = (roles) => {
   const allowedRoles = roles.split('||').map((role) => role.trim());
 
   return (req, res, next) => {
@@ -30,17 +30,17 @@ let authRole = (roles) => {
   };
 };
 
-let generateAccessToken = (user) => {
+const generateAccessToken = (user) => {
   return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
     expiresIn: '15min',
   });
 };
 
-let generateRefreshToken = (user) => {
+const generateRefreshToken = (user) => {
   return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
 };
 
-let verifyRefreshToken = (res, refreshToken) => {
+const verifyRefreshToken = (res, refreshToken) => {
   jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
     if (err) {
       return res.sendStatus(403);
