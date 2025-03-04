2afbf2cbeb5da84ef3912b293839698d4f2b1e54
kevinforter <49223606+kevinforter@users.noreply.github.com>
2025-03-05 00:13:38 +0100

    changed status messages

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,10 +4,12 @@ require('dotenv').config();
 const authToken = (req, res, next) => {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
-  if (token === null) return res.sendStatus(401);
+  if (token === null)
+    return res.status(401).json({ error: 'Access token not provided' });
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
+    if (err)
+      return res.status(403).json({ error: 'Invalid or expired access token' });
     req.user = user;
     next();
   });
