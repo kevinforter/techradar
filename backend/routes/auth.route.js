6ffb23c1b576e22ac844cb45a7acf2a6bacf84ae
kevinforter <49223606+kevinforter@users.noreply.github.com>
2025-03-05 00:14:33 +0100

    changed request method

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -11,6 +11,6 @@ const {
 router.post('/register', register);
 router.post('/login', login);
 router.delete('/logout', logout);
-router.get('/token', getToken);
+router.post('/token', getToken);
 
 module.exports = router;
