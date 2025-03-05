d7da191ad136ea4531f576d5418951ecd2c98875
kevinforter <49223606+kevinforter@users.noreply.github.com>
2025-03-05 01:23:30 +0100

    added tech modul

---
+++ frontend/src/app/tech.ts
@@ -0,0 +1,12 @@
+export interface Tech {
+  _id: string;
+  name: string;
+  ring: string;
+  category: string;
+  techDescription: string;
+  classificationDescription: string;
+  status: string;
+  createdAt: Date;
+  updatedAt: Date;
+  publicationDate: Date;
+}
