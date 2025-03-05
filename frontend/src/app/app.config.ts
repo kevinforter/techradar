cf56199453d4bc8be9a0c6d50d81789890d21d41
kevinforter <49223606+kevinforter@users.noreply.github.com>
2025-03-05 01:01:58 +0100

    added providers

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,8 +2,22 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
+//import { authInterceptorFn } from './interceptor/auth/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(withFetch() /*withInterceptors([authInterceptorFn])*/),
+  ],
 };
